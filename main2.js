a32f86cf9e5a7834ff6606e52e214203a69a1793
Damian Oscar Tarbuch <damianoscar.tarbuch@gmail.com>
2023-04-16 20:12:36 -0300

    Se configuró la función correctamente, se aplicó una variable true o false que fue aplicada como condicional para corregir el código

--- main2.js
+++ main2.js
@@ -5,6 +5,41 @@ const opcion_4 = 1000;
 const opcion_5 = 1000;
 const opcion_6 = 1000;
 
+const PRECIO = (segundaSeleccion) => {
+
+  switch (segundaSeleccion) {
+    case 1:
+      alert("Ud seleccinó el señor de los anillos");
+      return 1000;
+
+    case 2:
+      alert("Ud seleccionó Harry Potter");
+      return 1000;
+
+    case 3:
+      alert("Ud seleccionó Star wars");
+      return 1000;
+
+    case 4:
+      alert("Ud seleccionó Breaking bad");
+      return 1500;
+
+    case 5:
+      alert("Ud seleccionó Peaky Blinders");
+      return 1500;
+
+    case 6:
+      alert("Ud seleccionó Prison Break");
+      return 1500;
+
+    default:
+      alert("Ud no seleccionó ninguna opción correctamente");
+      break;
+  }
+}
+
+let continuarCompra = true;
+
 let ingresarUsuario = prompt("Por favor ingrese su nombre completo");
 
 alert("Bienvenido a la tienda virtual " + ingresarUsuario);
@@ -13,7 +48,7 @@ let consultaCompra = prompt(
   ingresarUsuario + ", Desea realizar una compra SI o NO?"
 );
 
-while (consultaCompra != "NO" && consultaCompra != "no") {
+while (consultaCompra != "NO" && consultaCompra != "no" && continuarCompra) {
   let primeraEleccion = prompt("Quiere comprar una serie o una película?");
 
   while (
@@ -58,51 +93,15 @@ while (consultaCompra != "NO" && consultaCompra != "no") {
     );
   }
 
-  function calcularPrecio(segundaSeleccion)
-
-  switch (segundaSeleccion) {
-    case 1:
-      alert("Ud seleccinó el señor de los anillos");
-      break;
-
-    case 2:
-      alert("Ud seleccionó Harry Potter");
-      break;
-
-    case 3:
-      alert("Ud seleccionó Star wars");
-      break;
-
-    case 4:
-      alert("Ud seleccionó Breaking bad");
-    
-      break;
-
-    case 5:
-      alert("Ud seleccionó Peaky Blinders");
-      break;
-
-    case 6:
-      alert("Ud seleccionó Prison Break");
-      break;
-
-    default:
-      alert("Ud no seleccionó ninguna opción correctamente");
-      break;
-  }
+  function calcularPrecio(segundaSeleccion);
 
   let consultaOtraCompra = prompt("Desea realizar otra compra?");
 
   if (consultaOtraCompra != "NO" && consultaOtraCompra != "No" && consultaOtraCompra != "no") {
     alert("Continuemos con la compra");
-    
-    segundaSeleccion = Number(
-      prompt(
-        "Seleccione el título que ud desee, escribiendo solo el número correspodiente al mismo según el siguiente listado: \n \n 1) El señor de los anillos \n 2) Harry Potter \n 3) Star wars \n 4) Breaking bad \n 5) Peaky blinders \n 6) Prison break"
-      )
-    );
 
   } else {
+    continuarCompra = false
     alert("Gracias por su compra " + ingresarUsuario + ". Hasta pronto")
   }
 }
